1d15d9f205af7c3540f2224005ae45d66b484682
CosmicCynth <theshinyseal@gmail.com>
2024-10-24 13:33:54 +0200

    Update Euler1.py

--- Euler1.py
+++ Euler1.py
@@ -1,1 +1,11 @@
-print("Hello world")
+Number = 1
+Numberliste = []
+
+while Number < 1000:
+    if Number%3==0 or Number%5==0:
+        Numberliste.append(Number)
+    Number = Number + 1
+
+print(Numberliste)
+Answer = sum(Numberliste)
+print(Answer)
